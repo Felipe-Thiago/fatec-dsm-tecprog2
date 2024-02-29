005b9225aab4942bbbaf81f03b07cf7442a5da67
Felipe Thiago <felipethiagodasilva@gmail.com>
2024-02-28 22:16:23 -0300

    Continuação exercício

--- calculaHoras/src/calculahoras/CalculaHoras.java
+++ calculaHoras/src/calculahoras/CalculaHoras.java
@@ -5,14 +5,132 @@ package calculahoras;
  * @author Alunos
  */
 public class CalculaHoras {
+    //Dados
+    private int entrada = 0;
+    private int saida = 0;
+    
+    private int dataInicio = 0;
+    private int dataFim = 0;
+    
 
-    /**
-     * @param args the command line arguments
-     */
+    public int getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(int entrada) {
+        this.entrada = entrada;
+    }
+
+    public int getSaida() {
+        return saida;
+    }
+
+    public void setSaida(int saida) {
+        this.saida = saida;
+    }
+
+    public int getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(int dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public int getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(int dataFim) {
+        this.dataFim = dataFim;
+    }
     
     
-    public static void main(String[] args) {
-        // TODO code application logic here
+    //Métodos
+    public int retornaHoras(int entrada, int saida){
+        int horas = saida - entrada - 1;
+        
+        return horas;
+        
+    }
+    
+    public int retornaHorasDias(int entrada, int saida, int dataInicio, int dataFim){
+        int horas = saida - entrada - 1;
+        int dias = dataFim - dataInicio + 1;
+        
+        int horasTotais = horas * dias;
+        
+        return horasTotais;
+    }
+    
+    
+    public int retornaHorasSemana(int entrada, int saida, int dataInicio, int dataFim, String diaSemana){
+        int horas = saida - entrada - 1;
+        int dias = dataFim - dataInicio + 1;
+        int horasTotais = horas * dias;
+        int total = 0;
+        
+        for(int i = dataInicio; i >= dataFim; i++){
+            switch(diaSemana){
+            case "Segunda":
+            case "Terça":
+            case "Quarta":
+            case "Quinta":
+            case "Sexta":
+                total = total + horas;
+                /////////////////////////////
+                
+            case "Sábado":
+            case "Domingo":
+                horasTotais = horasTotais + horasTotais/2;
+                return horasTotais;
+            default:
+                return horasTotais;
+        }
+        }
+        
+        switch(diaSemana){
+            case "Segunda":
+            case "Terça":
+            case "Quarta":
+            case "Quinta":
+            case "Sexta":
+                if (entrada < 8){
+                    int extra = horas + (8 - entrada)/2;
+                    horasTotais = horasTotais + extra;
+                }
+                if (saida > 18){
+                    int extra = horas + (saida - 18)/2;
+                    horasTotais = horasTotais + extra;
+                }
+                return horasTotais;
+                
+                
+            case "Sábado":
+            case "Domingo":
+                horasTotais = horasTotais + horasTotais/2;
+                return horasTotais;
+            default:
+                return horasTotais;
+        }
+        
+        
+    }
+    
+    public int retornaHorasNoturno(int entrada, int saida, int dataInicio, int dataFim){
+        int horas = saida - entrada - 1;
+        int dias = dataFim - dataInicio + 1;
+        
+        int horasTotais = horas * dias;
+        
+        if(entrada > saida){ //20 -> 04:00
+            horas = horas + 24;
+            if(saida < 6){
+                 
+            }
+        } 
+        
+        return horasTotais;
     }
     
 }
--- calculaHoras/test/testeCalculaHoras.java
+++ calculaHoras/test/testeCalculaHoras.java
@@ -10,7 +10,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+import calculahoras.*;
 
 
 /**
@@ -18,81 +18,40 @@ import static org.junit.Assert.*;
  * @author Alunos
  */
 public class testeCalculaHoras {
-    public int entrada = 8;
-    public int saida = 17;
     
-    public int dataInicio = 01;
-    public int dataFim = 30;
+    CalculaHoras calc = new CalculaHoras();
     
-    public int retornaHoras(int entrada, int saida, int dataInicio, int dataFim){
-        int horas = saida - entrada - 1;
-        int dias = dataFim - dataInicio;
-        int horasTotais = horas * dias;
-        
-        return horas;
-        
-    }
-    
-    public int retornaHorasTotais(int entrada, int saida, int dataInicio, int dataFim, String diaSemana){
-        int horas = saida - entrada - 1;
-        int dias = dataFim - dataInicio;
-        int horasTotais = horas * dias;
-        
-        switch(diaSemana){
-            case "Segunda":
-            case "Terça":
-            case "Quarta":
-            case "Quinta":
-            case "Sexta":
-                if (entrada < 8){
-                    int extra = horas + (8 - entrada)/2;
-                    horasTotais = horasTotais + extra;
-                }
-                if (saida > 18){
-                    int extra = horas + (saida - 18)/2;
-                    horasTotais = horasTotais + extra;
-                }
-                return horasTotais;
-                
-                
-            case "Sábado":
-            case "Domingo":
-                horasTotais = horasTotais + horasTotais/2;
-                return horasTotais;
-            default:
-                return horasTotais;
-        }
-        
-        
-    }
       
     //Método para calcular horas considerando 1 hora de almoço
     @Test
     public void calculaHorasTotais(){
-    int horas = saida - entrada - 1;
+    int horas = calc.retornaHoras(8, 17);
     
     
-        System.out.println("Você trabalhou por " + horas + " horas hoje");
+        System.out.println("1. Você trabalhou por " + horas + " horas hoje");
     }
 
     //Método para calcular horas com a diferença entre dias
     //Ex: trabalhou do dia 02 ao dia 05 das 05:00 às 14:00
     @Test
     public void calculaHorasEmDias(){
-        int horas = saida - entrada - 1;
+        int horasTotais = calc.retornaHorasDias(5, 14, 2, 5);
         
-        int dias = dataFim - dataInicio;
-        
-        int horasTotais = horas * dias;
-        
-        System.out.println("Você trabalhou, ao todo, por " + horasTotais + " horas");
+        System.out.println("2. Você trabalhou, ao todo, por " + horasTotais + " horas");
         
     }
-    
+        
     //Método para calcular horas considerando o dia da semana
+    //Ex: trabalhou das 8 às 17 do dia 1 ao 30 "como o horário de sábado"
     @Test
     public void calculaHorasEmDiasComDiaSemana(){
-        int horasTrabalhadas = retornaHorasTotais(8, 17, 1, 30, "Sábado");
-        System.out.println("Você receberá por " + horasTrabalhadas + " horas neste mês");
+        int horasTrabalhadas = calc.retornaHorasSemana(8, 17, 1, 30, "Sábado");
+        System.out.println("3. Você receberá por " + horasTrabalhadas + " horas neste mês");
+    }
+    
+    //Método para calcular horas com adicional noturno
+    @Test
+    public void calculaHorasEmDiasComDiaSemanaComNoturno(){
+        int horasTrabalhadas = 
     }
 }
